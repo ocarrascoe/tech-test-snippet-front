9167df74442382ccfdad0dccfd42f96b176dd424
ocarrascoe <omar.carrascoe@gmail.com>
2022-05-23 17:47:22 +0200

    Upd: Base Architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import * as React from 'react';
-import Dashboard from "./Dashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes as appRouter } from "./routes";
+import {DashboardLayout} from "./views";
 
 export default function App() {
   return (
-    <Dashboard/>
+    <Router>
+      <DashboardLayout>
+        <Routes>
+          {appRouter.map((route) => (
+            <Route
+              key={route.key}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+        </Routes>
+      </DashboardLayout>
+    </Router>
   );
 }
--- src/views/layouts/DashboardLayout.tsx
+++ src/views/layouts/DashboardLayout.tsx
@@ -1,39 +1,22 @@
 import * as React from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Link from '@mui/material/Link';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems } from './listItems';
-import Chart from './Chart';
-import Deposits from './Deposits';
-import Orders from './Orders';
+import {FC} from 'react';
+import Typography from "@mui/material/Typography";
+import {AppBarProps as MuiAppBarProps} from "@mui/material/AppBar/AppBar";
+import {styled} from "@mui/material/styles";
+import MuiAppBar from "@mui/material/AppBar";
+import MuiDrawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Badge from "@mui/material/Badge";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import Box from "@mui/material/Box";
+import {ItemList} from "../../components/ui";
 
-function Copyright(props: any) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const drawerWidth: number = 240;
 
@@ -43,7 +26,7 @@ interface AppBarProps extends MuiAppBarProps {
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
+})<AppBarProps>(({theme, open}) => ({
   zIndex: theme.zIndex.drawer + 1,
   transition: theme.transitions.create(['width', 'margin'], {
     easing: theme.transitions.easing.sharp,
@@ -59,8 +42,8 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
+const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})(
+  ({theme, open}) => ({
     '& .MuiDrawer-paper': {
       position: 'relative',
       whiteSpace: 'nowrap',
@@ -85,18 +68,20 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
-const mdTheme = createTheme();
 
-function DashboardContent() {
+interface Props {
+  children?: React.ReactNode;
+}
+
+export const DashboardLayout: FC<Props> = ({children}) => {
   const [open, setOpen] = React.useState(true);
   const toggleDrawer = () => {
     setOpen(!open);
   };
-
   return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
+    <>
+      <Box sx={{display: 'flex'}}>
+        <CssBaseline/>
         <AppBar position="absolute" open={open}>
           <Toolbar
             sx={{
@@ -110,25 +95,25 @@ function DashboardContent() {
               onClick={toggleDrawer}
               sx={{
                 marginRight: '36px',
-                ...(open && { display: 'none' }),
+                ...(open && {display: 'none'}),
               }}
             >
-              <MenuIcon />
+              <MenuIcon/>
             </IconButton>
             <Typography
               component="h1"
               variant="h6"
               color="inherit"
               noWrap
-              sx={{ flexGrow: 1 }}
+              sx={{flexGrow: 1}}
             >
-              Dashboard
+              Library
             </Typography>
-            <IconButton color="inherit">
+            {/*<IconButton color="inherit">
               <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
+                <NotificationsIcon/>
               </Badge>
-            </IconButton>
+            </IconButton>*/}
           </Toolbar>
         </AppBar>
         <Drawer variant="permanent" open={open}>
@@ -141,14 +126,12 @@ function DashboardContent() {
             }}
           >
             <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
+              <ChevronLeftIcon/>
             </IconButton>
           </Toolbar>
-          <Divider />
+          <Divider/>
           <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
+            <ItemList/>
           </List>
         </Drawer>
         <Box
@@ -163,10 +146,11 @@ function DashboardContent() {
             overflow: 'auto',
           }}
         >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Toolbar/>
+          {children}
+          {/*<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
-              {/* Chart */}
+               Chart
               <Grid item xs={12} md={8} lg={9}>
                 <Paper
                   sx={{
@@ -179,7 +163,7 @@ function DashboardContent() {
                   <Chart />
                 </Paper>
               </Grid>
-              {/* Recent Deposits */}
+               Recent Deposits
               <Grid item xs={12} md={4} lg={3}>
                 <Paper
                   sx={{
@@ -192,7 +176,7 @@ function DashboardContent() {
                   <Deposits />
                 </Paper>
               </Grid>
-              {/* Recent Orders */}
+               Recent Orders
               <Grid item xs={12}>
                 <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
                   <Orders />
@@ -200,13 +184,11 @@ function DashboardContent() {
               </Grid>
             </Grid>
             <Copyright sx={{ pt: 4 }} />
-          </Container>
+          </Container>*/}
         </Box>
       </Box>
-    </ThemeProvider>
-  );
+    </>
+  )
 }
 
-export default function Dashboard() {
-  return <DashboardContent />;
-}
+
